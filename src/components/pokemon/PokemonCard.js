669bd79e75ed190f1f3e77e0ec2d661e8b271284
SteffenEBraaten <steffen.e.braaten@gmail.com>
2020-05-13 21:48:02 +0200

    Installed Axios and set up connection to the PokeAPI.

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class PokemonCard extends Component {
+    state = {
+        name: '',
+        imageUrl: '',
+        pokemonIndex: '',
+    };
+
+    componentDidMount() {
+        const {name, url} = this.props;
+        const pokemonIndex = url.split('/')[url.split('/').length - 2];
+        const imageUrl = `https://github.com/PokeAPI/sprites/tree/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+    
+        console.log(imageUrl)
+        this.setState({
+            name,
+            imageUrl,
+            pokemonIndex
+        })
+    }
+
     render() {
+        
         return (
             <div
             className='col-md-3 col-sm-6 mb-5'>
                 <div className="card">
-                    <div className="card-header">
-                        <h1>Bulbasaur</h1>
+                    <h3 className="card-header">
+                        {this.state.pokemonIndex}
+                    </h3>
+                    <div className="card-body mx-auto">
+                        <h4 className="card-title">
+                            {this.state.name
+                            .toLowerCase()
+                            .split(' ')
+                            .map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+                            .join(' ')}
+                            </h4>
                     </div>
                 </div>
             </div>
