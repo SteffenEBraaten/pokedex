4ae09e6656286c5f109fd7127806a53a1750ae29
SteffenEBraaten <steffen.e.braaten@gmail.com>
2020-05-21 15:26:57 +0200

    Increased the limit of retrived pokemon from 20 to 30

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -6,7 +6,7 @@ import PokemonCard from "./PokemonCard";
 export default class PokemonList extends Component {
 
     state = {
-        url: 'https://pokeapi.co/api/v2/pokemon/',
+        url: 'https://pokeapi.co/api/v2/pokemon/?limit=30',
         pokemon: null,
         next: null,
         prev: null
